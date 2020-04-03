ca0a17eefdb397dfe38cc848534f748766b48961
austinb847 <austinb847@icloud.com>
2020-04-03 10:00:26 -0700

    add updated while loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,17 @@
 //Back end logic
 var toRomanNumeral = function(number) {
   if (number > 0 && number <= 3999) {
-    var romanNumeralsDict = {"M":1000, "D":500, "C":100, "L":50, "X":10, "V":5, "I":1};
-    var roman = [];
-    var keys = Object.keys(romanNumeralsDict);
-    var currValue;
-    var index;
-    var count = 1;
-    
-    for (var numeral in romanNumeralsDict) {
-      currValue = romanNumeralsDict[numeral];
-      index = keys.indexOf(numeral);
+    var romanNumeralsDict = {M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V:5, IV: 4, I: 1};
+    var roman = "";
 
-      while(number >= currValue) {
-        if(count < 4) {
-          roman.push(numeral)
-        }
+    for (var key in romanNumeralsDict) {
+      /* console.log(key);
+      console.log(romanNumeralsDict[key]); */
+      while(number >= romanNumeralsDict[key]) {
+        roman = roman + key;
+        number = number - romanNumeralsDict[key];
       }
     }
-    
-    
     
     return roman;
 
