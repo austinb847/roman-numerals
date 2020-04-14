d7437b970d7f7dae5e9685c56a4849fbb398b1e5
austinb847 <austinb847@icloud.com>
2020-04-14 15:18:21 -0700

    get meaningful fail when testing toRomanNumeral function to make sure function call works

---
+++ __tests__/roman-numerals.test.js
@@ -0,0 +1,15 @@
+import { toRomanNumeral } from './../src/roman-numerals.js';
+
+describe('toRomanNumeral' , () => {
+
+test('should correctly call toRomanNumeral function and take in a number', () => {
+  var romanNum = toRomanNumeral(2);
+  expect(romanNum).toEqual(2);
+});
+
+
+
+
+
+
+});
--- src/roman-numerals.js
+++ src/roman-numerals.js
@@ -1,11 +1,9 @@
 export var toRomanNumeral = function(number) {
-  if (number > 0 && number <= 3999) {
+  /* if (number > 0 && number <= 3999) {
     var romanNumeralsDict = {M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V:5, IV: 4, I: 1};
     var roman = "";
 
     for (var key in romanNumeralsDict) {
-      /* console.log(key);
-      console.log(romanNumeralsDict[key]); */
       while(number >= romanNumeralsDict[key]) {
         roman = roman + key;
         number = number - romanNumeralsDict[key];
@@ -16,5 +14,6 @@ export var toRomanNumeral = function(number) {
 
   } else {
     return false;
-  }
+  } */
+  
 };
