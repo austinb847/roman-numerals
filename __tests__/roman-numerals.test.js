3b9b6956711df7fe451664fe3309e5bd7e190959
austinb847 <austinb847@icloud.com>
2020-04-14 16:58:17 -0700

    implement successful test 'roman-numerals.test' for toRomanNumerals function. test sucessfull

--- __tests__/roman-numerals.test.js
+++ __tests__/roman-numerals.test.js
@@ -8,11 +8,14 @@ test('should correctly call toRomanNumeral function and take in a number', () =>
 });
 
 test('should take in a number between 0 and 3999', () => {
-  var romanNum = toRomanNumeral(4000);
-  expect(romanNum).toEqual(4000);
+  var romanNum = toRomanNumeral(400);
+  expect(romanNum).toEqual(400);
 });
 
-
+test('should return false for numbers that are not between 0 and 3999', () => {
+  var romanNum = toRomanNumeral(4000);
+  expect(romanNum).toEqual(false);
+});
 
 
 });
